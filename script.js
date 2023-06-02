6879c40b6b7b4136a165adb45b200a4763081958
beb97 <beb97@hotmail.com>
2023-06-03 00:01:14 +0200

    updated title

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function toogleTime(el) {
   el.classList.toggle("night");
+  document.querySelector("html").classList.toggle("dark-mode");
+  (document.title=="jour")?document.title="nuit":document.title="jour";
 }
